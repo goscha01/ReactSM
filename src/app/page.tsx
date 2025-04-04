edbabff316b08f3916ef930240daacf6f534337e
goscha01 <sayapingeorge@gmail.com>
2025-04-04 15:25:30 -0400

    removed adsense component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,6 @@ export default function MainPage() {
           }`}
       >
         <NavbarSection />
-        <GoogleAdsense />
         <CanvasWrapper />
         <InstructionsSection />
         <AboutSection />
