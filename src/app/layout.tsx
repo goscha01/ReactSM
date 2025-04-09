16eb2c040aefc88b6ada3cd5cf2373b0f1148d4c
goscha01 <sayapingeorge@gmail.com>
2025-04-09 18:33:37 -0400

    refactored layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,9 @@ import localFont from "next/font/local";
 import { } from "next/font/google";
 import "./globals.css";
 import MainContextProvider from "@/components/MainContext";
-
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google';
 import GoogleAdsense from "@/components/GoogleAdsense";
-
+import Head from 'next/head';  // Import next/head
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,8 +17,6 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
-
-
 
 export const metadata: Metadata = {
   title: "My Stamp Maker",
@@ -34,19 +31,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Add Google AdSense Script Here */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9305828682531722"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
 
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <MainContextProvider>
-         <GoogleAdsense />
+          <GoogleAdsense />
           {children}
         </MainContextProvider>
         <GoogleAnalytics gaId="G-WV0S24L3GV" />
-
       </body>
-
     </html>
   );
 }
