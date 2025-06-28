8842ed7ee6630a44fd40f61dc24a8fa7ccf14379
goscha01 <sayapingeorge@gmail.com>
2025-06-28 16:51:20 -0400

    returned to the first approach with build

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: "export",  // Static export configuration
   trailingSlash: true,  // Ensures that URLs end with a trailing slash
+  distDir: 'build',  // Output directory (you'll upload this folder to S3)
   images: {
     unoptimized: true,  // Disable image optimization for static export 
   },
